98a83d7515661c61dd0b1544b9ba8194a27a9b52
Oksana <oks.mykhailiuk@gmail.com>
2019-01-02 15:40:42 +0200

    node-practic1-data from input on anothr page like object

--- app.js
+++ app.js
@@ -2,44 +2,75 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
-fs.writeFile('./index.html','my index',(err)=>{
-    if (!err) console.log('ok');
-});
-fs.writeFile('./info.html','my info',{flags:'a'},(err)=>{
-    if (!err) console.log('ok');
-});
-fs.writeFile('./users.html','my users',(err)=>{
-    if (!err) console.log('ok');
-});
-fs.writeFile('./404.html','error 404',(err)=>{
-    if (!err) console.log('ok');
-});
+// fs.writeFile('./index.html','my index',(err)=>{
+//     if (!err) console.log('ok');
+// });
+// fs.writeFile('./info.html','my info',(err)=>{
+//     if (!err) console.log('ok');
+// });
+// fs.writeFile('./users.html','my users',(err)=>{
+//     if (!err) console.log('ok');
+// });
+// fs.writeFile('./404.html','error 404',(err)=>{
+//     if (!err) console.log('ok');
+// });
+//
+// http.createServer((req, res) => {
+//
+//     let urlParse = url.parse(req.url);
+//     console.log(urlParse);
+//
+//     switch (urlParse.pathname) {
+//         case '/':
+//             fs.readFile('./index.html', (err, data) => {
+//                 res.end(data)
+//             });
+//             break;
+//         case '/info':
+//             fs.readFile('./info.html', (err, data) => {
+//                 res.end(data)
+//             });
+//             break;
+//         case '/users':
+//             fs.readFile('./users.html', (err, data) => {
+//                 res.end(data)
+//             });
+//             break;
+//         default: fs.readFile('./404.html',(err,data)=>{
+//             res.end(data)
+//         });
+//     };
+// }).listen(3000, (err) => {
+//     if (!err) console.log('listen 3000');
+// })
 
-http.createServer((req, res) => {
+let server = http.createServer((req, res) => {
 
-    let urlParse = url.parse(req.url);
-    console.log(urlParse);
-
-    switch (urlParse.pathname) {
+    let parsedUrl = url.parse(req.url,true);
+    switch (parsedUrl.pathname) {
         case '/':
-            fs.readFile('./index.html', (err, data) => {
-                res.end(data)
-            });
+        {
+            let readStream = fs.createReadStream('./index.html');
+            readStream.pipe(res);
             break;
-        case '/info':
-            fs.readFile('./info.html', (err, data) => {
-                res.end(data)
-            });
+        }
+        case '/info': {
+            // console.log(parsedUrl.query);
+            res.end(JSON.stringify(parsedUrl.query));
             break;
-        case '/users':
-            fs.readFile('./users.html', (err, data) => {
-                res.end(data)
-            });
-            break;
-        default: fs.readFile('./404.html',(err,data)=>{
-            res.end(data)
-        });
-    };
-}).listen(3000, (err) => {
+        }
+        default:{
+            let readStream = fs.createReadStream('./404.html');
+            readStream.pipe(res,)
+        }
+
+    }
+});
+
+server.listen(3000, (err) => {
     if (!err) console.log('listen 3000');
-})
+});
+
+
+
+
